92be85a1d02747688e2c0cbf375028c6f668f33d
gorodnyuk <sergeygorodnyuk@yandex.ru>
2024-02-11 21:23:06 +0300

    Init Commit

---
+++ src/main/java/ru/gorodnyuk/digitalgrocery/DigitalGroceryApplication.java
@@ -0,0 +1,13 @@
+package ru.gorodnyuk.digitalgrocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalGroceryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalGroceryApplication.class, args);
+    }
+
+}
