733cd440bbf708562421166fc8ebf93c185265c0
gorodnyuk <sergeygorodnyuk@yandex.ru>
2024-02-12 20:29:59 +0300

    Add materialize

--- src/main/java/ru/gorodnyuk/digitalgrocery/model/Product.java
+++ src/main/java/ru/gorodnyuk/digitalgrocery/model/Product.java
@@ -1,25 +1,29 @@
 package ru.gorodnyuk.digitalgrocery.model;
 
+import lombok.Getter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.math.BigDecimal;
 
+@Getter
 @Entity
 @Table(name = "products")
 public class Product {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name", nullable = false, length = 2000)
     private String name;
 
-    @Column(name = "cost", nullable = false)
-    private BigDecimal cost;
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
 
     @Column(name = "balance", nullable = false)
     private Integer balance;
--- src/main/java/ru/gorodnyuk/digitalgrocery/web/MainController.java
+++ src/main/java/ru/gorodnyuk/digitalgrocery/web/MainController.java
@@ -2,6 +2,7 @@ package ru.gorodnyuk.digitalgrocery.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.gorodnyuk.digitalgrocery.service.ProductService;
 
@@ -12,7 +13,8 @@ public class MainController {
     private final ProductService productService;
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("products", productService.getAllProducts());
         return "index";
     }
 }
